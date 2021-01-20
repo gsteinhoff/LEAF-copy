15916e0b87869cbe3a333c8c95fe0d40467fca54
Pete Nerantzinis <dimach44@gmail.com>
2021-01-20 12:58:05 -0500

    Create globals.php

---
+++ LEAF_Nexus/globals.php
@@ -0,0 +1,17 @@
+<?php
+const PRODUCT_NAME = 'VA Light Electronic Action Framework';
+const VERSION_NUMBER = '4.0.0';
+
+const DIRECTORY_HOST = 'mysql';
+const DIRECTORY_DB = 'leaf_users';
+const DIRECTORY_USER = 'tester';
+const DIRECTORY_PASS = 'tester';
+
+const LEAF_NEXUS_URL = 'https://localhost/LEAF_Nexus/'; // trailing slash required
+const HTTP_HOST = 'www.example.com';
+
+const AUTH_URL = 'example.com/auth';
+
+const AUTH_TYPE = 'cookie';
+
+const CIPHER_KEY = 'example-key';
